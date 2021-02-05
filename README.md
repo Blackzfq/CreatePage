743605a7a687633d7a0e64bf7d6c5cd370597caa
zhangfangqing <7377019+zhang_fang_qing@user.noreply.gitee.com>
2021-02-05 10:53:14 +0800

    文档修改

--- README.md
+++ README.md
@@ -1,21 +1,27 @@
-# bluecreate
+# CreatePages
 
 ## Build Setup
 
 ```bash
-# 项目安装
+# install dependencies
 $ npm install
 
-# 项目运行 本地地址 localhost:3000
-$ npm dev
+# serve with hot reload at localhost:3000
+$ npm run dev
 
-# 项目打包和启动
-$ npm build
-$ npm start
+# build for production and launch server
+$ npm run build
+$ npm run start
 
-# 生成SPA静态项目
-$ npm generate
+# generate static project
+$ npm run generate
 ```
+## 文档说明
 
-# 项目文档说明
+```bash
+# 尝试着做一个能让零编程基础的人能够自由定制网页的工具 
+# 功能雷同目前市面上的低代码平台
+# 目前处于开发阶段，会不定时更新进展，算给自己开一个坑，希望2021年能够完成
 
+# 项目是基于Vue的Nuxt.js 2.x进行开发的
+# 因为既然是自由定制网页,SEO方面也希望有很好的支持，本人学识有限目前还没法完全参透Vue SSR的情况，直接开箱即用了NUXT 望海涵
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'bluecreate',
+    title: 'CreatePages',
     htmlAttrs: {
       lang: 'en'
     },
@@ -17,10 +17,12 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    'ant-design-vue/dist/antd.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '@/plugins/antd-ui'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -28,14 +30,10 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
